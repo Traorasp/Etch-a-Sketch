553352efca68d9ebf8a0459017dfe8a2a55c61a4
Traorasp <miguelsolanopaz@outlook.com>
2022-04-30 02:56:10 -0400

    Creater New Grid button to create a new grid with a size equal to what the user imputs

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,13 +1,38 @@
 const container = document.querySelector('#container');
 
-for(let i = 0; i < 16; i++){
-    let row = document.createElement('div');
-    row.classList.add('row')
-    for(let k = 0; k < 16; k++){
-        let col = document.createElement('div');
-        col.classList.add('row');
-        row.appendChild(col);
+const newGrid = document.querySelector('#newGridButton');
+
+newGrid.addEventListener('click', createGrid);
+
+createGrid()
+
+function createGrid(){
+    removeGrid();
+    let size = prompt('Inpiut the size of your new grid')
+    if (size > 100) {size = 100};
+    for(let i = 0; i < size; i++){
+        let row = document.createElement('div');
+        row.classList.add('row')
+        for(let k = 0; k < size; k++){
+            let col = document.createElement('div');
+            col.classList.add('cell');
+            row.appendChild(col);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
+
+    const cells = Array.from(document.querySelectorAll('.cell'));
+
+    cells.forEach(cell => cell.addEventListener('mouseover', paintCell));
 }
+
+function removeGrid(){
+    const cells = Array.from(document.querySelectorAll('.cell'));
+    const rows = Array.from(document.querySelectorAll('.row'));
+rows.forEach(row => row.remove());
+cells.forEach(cell => cell.remove());
+
+}
+
+function paintCell(cell) {cell.target.classList.add('painted')};
 
